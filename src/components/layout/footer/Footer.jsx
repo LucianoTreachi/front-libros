77a0a58178f2761da817fa8cc541f96f3e5498ad
lucianotreachi <lucianotreachi@outlook.com>
2024-10-27 19:42:46 -0300

    Agregar símbolo de copyright y créditos al creador del sitio web

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -4,7 +4,20 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <p className={styles.paragraph}>FrontLibros</p>
+        <p className={styles.paragraph}>© FrontLibros</p>
+        <br />
+        <p className={styles.paragraph}>
+          Desarrollado por{" "}
+          <a
+            href="https://www.linkedin.com/in/luciano-treachi/"
+            className={styles.link}
+            aria-label="Visitar LinkedIn de Luciano Treachi, desarrollador del sitio web."
+            target="_blank"
+            rel="noreferrer"
+          >
+            Luciano Treachi
+          </a>
+        </p>
       </div>
     </footer>
   );
