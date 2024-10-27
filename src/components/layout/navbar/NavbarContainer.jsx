b497c2f9fc0cc83469c31c13f0d910a6cd5e02a8
lucianotreachi <lucianotreachi@outlook.com>
2024-10-27 16:05:49 -0300

    Realizar refactor y agregar comentarios breves y explicativos

--- src/components/layout/navbar/NavbarContainer.jsx
+++ src/components/layout/navbar/NavbarContainer.jsx
@@ -2,25 +2,25 @@ import { useCallback, useEffect, useState } from "react";
 import Navbar from "./Navbar";
 
 export default function NavbarContainer() {
+  // Estado para controlar el menú de navegación
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  // Alternar el estado de visibilidad del menú de navegación
+  const toggleMenu = () => setIsOpen((prev) => !prev);
 
+  // Función para cerrar el menú de navegación cuando se hace scroll
   const handleScroll = useCallback(() => {
     if (isOpen) {
       setIsOpen(false);
     }
   }, [isOpen]);
 
+  // Cerrar el menú de navegación al hacer scroll en la página
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    return () => window.removeEventListener("scroll", handleScroll);
   }, [handleScroll]);
 
+  // Renderizar el componente Navbar
   return <Navbar isOpen={isOpen} toggleMenu={toggleMenu} />;
 }
