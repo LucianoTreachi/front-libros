496df6ff2f236792ce3da19fa83ddab735467797
lucianotreachi <lucianotreachi@outlook.com>
2024-10-27 20:11:54 -0300

    Cambiar button por Link

--- src/components/common/goHomeButton/GoHomeButton.jsx
+++ src/components/common/goHomeButton/GoHomeButton.jsx
@@ -1,13 +1,15 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { BsArrowLeft } from "react-icons/bs";
 import styles from "./GoHomeButton.module.css";
 
 export default function GoHomeButton() {
-  const navigate = useNavigate();
-
   return (
-    <button onClick={() => navigate("/")} className={styles.button}>
+    <Link
+      to="/"
+      className={styles.link}
+      aria-label="Volver a la página de inicio"
+    >
       <BsArrowLeft className={styles.icon} aria-hidden="true" /> Volver a inicio
-    </button>
+    </Link>
   );
 }
