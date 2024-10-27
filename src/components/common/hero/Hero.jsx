585aacaf786c363c6f476c9109482ad8da0b83d0
lucianotreachi <lucianotreachi@outlook.com>
2024-10-27 15:13:44 -0300

    Agregar componente Hero

---
+++ src/components/common/hero/Hero.jsx
@@ -0,0 +1,35 @@
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div>
+          <h1 className={styles.title}>
+            Descubre los Mejores Libros de Desarrollo Frontend
+          </h1>
+          <p className={styles.paragraph}>
+            Encuentra los recursos esenciales que necesitas para convertirte en
+            un experto del Desarrollo Frontend y crear sitios web
+            impresionantes.
+          </p>
+        </div>
+        <div>
+          <picture>
+            <source
+              media="(max-width: 574px)"
+              srcSet="https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/w_500/v1687112700/ecommerce-libros/mockup-libros_ri6oof.jpg"
+            />
+            <img
+              className={styles.image}
+              src="https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/v1687112700/ecommerce-libros/mockup-libros_ri6oof.jpg"
+              alt=""
+              width={1080}
+              height={704}
+            />
+          </picture>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from "react-helmet";
+import Hero from "../../common/hero/Hero";
 import ProductListContainer from "../productList/ProductListContainer";
-import styles from "./Home.module.css";
 
 export default function Home() {
   return (
@@ -13,35 +13,7 @@ export default function Home() {
       </Helmet>
 
       <main>
-        <section className={styles.section}>
-          <div className={styles.container}>
-            <div>
-              <h1 className={styles.title}>
-                Descubre los Mejores Libros de Desarrollo Frontend
-              </h1>
-              <p className={styles.paragraph}>
-                Encuentra los recursos esenciales que necesitas para convertirte
-                en un experto del Desarrollo Frontend y crear sitios web
-                impresionantes.
-              </p>
-            </div>
-            <div>
-              <picture>
-                <source
-                  media="(max-width: 574px)"
-                  srcSet="https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/w_500/v1687112700/ecommerce-libros/mockup-libros_ri6oof.jpg"
-                />
-                <img
-                  className={styles.image}
-                  src="https://res.cloudinary.com/dwqk2mkgh/image/upload/f_auto,q_auto/v1687112700/ecommerce-libros/mockup-libros_ri6oof.jpg"
-                  alt=""
-                  width={1080}
-                  height={704}
-                />
-              </picture>
-            </div>
-          </div>
-        </section>
+        <Hero />
         <ProductListContainer />
       </main>
     </>
