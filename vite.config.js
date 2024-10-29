0e11044982ed05c1ae6e56352c2efb59fc60720b
lucianotreachi <lucianotreachi@outlook.com>
2024-10-29 12:06:56 -0300

    Configurar múltiples alias en vite.config.js para acceder a components, routes y context

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      // Alias para la carpeta components
+      "@components": fileURLToPath(new URL('./src/components', import.meta.url)),
+      // Alias para la carpeta routes
+      "@routes": fileURLToPath(new URL('./src/routes', import.meta.url)),
+      // Alias para la carpeta context
+      "@context": fileURLToPath(new URL('./src/context', import.meta.url)),
+    },
+  },
   plugins: [react()],
-})
+});
